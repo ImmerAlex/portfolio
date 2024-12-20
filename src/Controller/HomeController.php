62f9b80eb95d7e9778898ea53ce8959f6074bcad
ImmerAlex <aleximm67@gmail.com>
2024-12-20 14:47:14 +0100

    complete logic

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,23 +8,30 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
+    private $openDate;
+
+    public function __construct()
+    {
+        $this->openDate = new \DateTime('2025-01-31T00:00:00+01:00');
+    }
+
     #[Route('/', name: 'root')]
     public function index(): Response
     {
-        $openDate = new \DateTime('2025-01-31T00:00:00+01:00');
-        $now = new \DateTime();
-        $diff = $openDate->diff($now);
-
-        if ($diff->invert) {
+        if ($this->openDate > new \DateTime()) {
             return $this->render('home/under-constructi.html.twig');
-        } else {
-            return $this->redirectToRoute('home');
         }
+
+        return $this->redirectToRoute('home');
     }
 
     #[Route('/home', name: 'home')]
     public function home(): Response
     {
+        if ($this->openDate > new \DateTime()) {
+            return $this->redirectToRoute('root');
+        }
+
         return $this->render('home/index.html.twig');
     }
 }
